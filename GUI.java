ebf97a1aab29c28286abbc75b33bf657042820d6
rspacerr <definitelynotoberon@gmail.com>
2022-02-06 18:12:53 -0500

    first commit

---
+++ GUI.java
@@ -0,0 +1,82 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI implements ActionListener {
+    private JFrame frame;
+    private JPanel panel;
+    private JTextField textfield;
+    private int x;
+    private int y;
+    private JLabel label;
+    private JButton submit;
+    private JLabel correct = new JLabel("");
+    private JLabel streak = new JLabel("");
+    private int count;
+
+    public GUI() {
+        frame = new JFrame();
+        panel = new JPanel();
+        textfield = new JTextField();
+        x = (int)(Math.random() * 30 + 1);
+        y = (int)(Math.random() * 30 + 1);
+        label = new JLabel(x + " + " + y);
+        submit = new JButton("Submit");
+
+        label.setBounds(170, 20, 80, 25);
+        panel.add(label);
+
+        textfield.setBounds(105, 60, 165, 25);
+        panel.setLayout(null);
+        panel.add(textfield);
+
+        submit.setBounds(150, 100, 80, 20);
+        submit.addActionListener(this);
+        panel.add(submit);
+        
+        correct.setBounds(150, 120, 150, 25);
+        streak.setBounds(150, 140, 150, 25);
+        streak.setText("Current streak: " + count);
+        panel.add(correct);
+        panel.add(streak);
+        
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setSize(350, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setTitle("Math Pacer");
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+
+    public String getTextfield() {
+        return textfield.getText();
+    }
+
+    public void generateNumbers() {
+        x = (int)(Math.random() * 30 + 1);
+        y = (int)(Math.random() * 30 + 1);
+        label.setText(x + " + " + y);
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (Integer.parseInt(getTextfield()) == (x+y)) {
+            correct.setText("Correct!");
+            count++;
+        }
+        else {
+            correct.setText("Incorrect! The correct answer was " + (x+y));
+            count = 0;
+        }
+        textfield.setText("");
+        generateNumbers();
+        streak.setText("Current streak: " + count);
+    }
+}
+
---
+++ ListenForKeys.java
@@ -0,0 +1,22 @@
+/*
+import java.awt.event.*;
+
+public class ListenForKeys implements KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+
+    public void submitAnswer(KeyEvent e, GUI g) {
+        char pressed = e.getKeyChar();
+
+        if (pressed == '\n' && Integer.parseInt(g.getTextfield()) == g.getAnswer()) {
+            System.out.println("Correct!");
+        }
+    }
+}
+*/
